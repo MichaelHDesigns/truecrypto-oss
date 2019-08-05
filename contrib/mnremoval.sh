23d153dff78dc26208d4c7803abea6b8faaa05ff
Alan Rudolf <suprnurd@gmail.com>
2019-08-05 16:31:28 -0400

    added simple uninstallation companion script

---
+++ contrib/mnremoval.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# truecrypto-oss auto uninstaller for mninstallation script
+# https://raw.githubusercontent.com/truedividendcryptocurrency/truecrypto-oss/development/contrib/mnremoval.sh
+
+CONFIG_FOLDER='/root/.tdc'
+COIN_DAEMON='tdcd'
+COIN_CLI='tdc-cli'
+COIN_TX='tdc-tx'
+COIN_PATH='/usr/local/bin/'
+COIN_NAME='truecrypto-oss'
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+if [[ $EUID -ne 0 ]]; then
+  echo -e "${RED}$0 must be run as root.${NC}"
+  exit 1
+fi
+
+if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMON" ] || [ -e "/etc/systemd/system/$COIN_NAME.service" ]; then
+  echo -e "Stopping $COIN_NAME masternode service..."
+  systemctl stop $COIN_NAME.service
+  echo -e "Removing $COIN_NAME masternode service..."
+  systemctl disable $COIN_NAME.service >/dev/null 2>&1
+  rm /etc/systemd/system/$COIN_NAME.service
+  systemctl daemon-reload
+  sleep 3
+  echo -e "Deleting $COIN_NAME binaries..."
+  rm $COIN_PATH$COIN_DAEMON
+  rm $COIN_PATH$COIN_CLI
+  rm $COIN_PATH$COIN_TX
+  echo -e "${GREEN}Done!${NC}"
+  exit 0
+else
+  echo -e "Cannot find ${RED}$COIN_NAME installation.${NC}"
+  exit 1
+fi
